a8d31101595c275f25a9f2f854cccaa9b29ac3a5
upender-01 <bhukya.upendar.ece23@itbhu.ac.in>
2026-05-31 11:40:30 +0530

    Fix formatting and update route definition

--- backend/routes/profileroutes.js
+++ backend/routes/profileroutes.js
@@ -1,9 +1,9 @@
-const express=require('express');
-const router=express.Router();
-const profileController=require('../controllers/profilecontroller');
+const express = require('express');
+const router = express.Router();
+
+const profileController = require('../controllers/profilecontroller');
 
 router.post('/analyze', profileController.analyzeprofile);
-router.get('/profiles', profileController.getprofileByUsername);
 router.get('/profiles/:username', profileController.getprofileByUsername);
 
-module.exports=router;
+module.exports = router;
