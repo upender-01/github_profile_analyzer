1a76a3c02975b1debc9e814ddd54fa1bd6893aea
upender-01 <bhukya.upendar.ece23@itbhu.ac.in>
2026-05-31 11:39:55 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,25 @@
-const express=require('express');
-const cors=require('cors');
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 
-const profileRoutes=require('./routes/profileroutes');
+const profileRoutes = require('./routes/profileroutes');
 
-const app=express();
+const app = express();
 
-app.use(cors({
-    origin : '*',
-    methods : ['GET', 'POST'],
-    allowedHeaders : ['Content-Type' , 'Authorization']
-}));
+app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
-app.get('/' , (req, res)=>{
-    res.send('Backend is running ');
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
 });
+
 app.use('/api', profileRoutes);
-app.use((err , req, res , next)=>{
-    console.error(err.stack);
-    res.status(500).json({error : 'Something broke!'});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({
+    error: 'Something went wrong',
+  });
 });
 
 const PORT = process.env.PORT || 3000;
