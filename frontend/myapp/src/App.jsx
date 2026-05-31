bf5185b8d5c2cdbe36e3c89af7eb973390fcbdfe
upender-01 <bhukya.upendar.ece23@itbhu.ac.in>
2026-05-31 11:28:33 +0530

    Update App.jsx

--- frontend/myapp/src/App.jsx
+++ frontend/myapp/src/App.jsx
@@ -16,15 +16,23 @@ function App() {
     setError('');
     setProfile(null);
 
-    try {
-      // Make sure your backend server.js is running on port 3000!
-      axios.post(
-  "https://github-profile-analyzer-ln7p.onrender.com/api/analyze",
-  data
-);
-      const response = await axios.post('https://github-profile-analyzer-ln7p.onrender.com/api/analyze', {
-       username: username.trim()  }
-      );
+   try {
+  const response = await axios.post(
+    "https://github-profile-analyzer-ln7p.onrender.com/api/analyze",
+    {
+      username: username.trim()
+    }
+  );
+
+  setProfile(response.data.data);
+}
+catch (err) {
+  if (err.response?.data?.error) {
+    setError(err.response.data.error);
+  } else {
+    setError("Failed to connect to the server.");
+  }
+}
       
       setProfile(response.data.data);
     } catch (err) {
